747c8d6472a746462fcbdf1110aadc639fd6ddca
shahriar <km.shahriar28@gmail.com>
2026-06-15 14:27:53 +0200

    initial commit

---
+++ OOP_task_ekvip/Program.cs
@@ -0,0 +1,14 @@
+﻿if (args.Length != 1)
+{
+    Console.WriteLine(" Please provide one initial number as a command-line argument." );
+    return;
+}
+
+int result = int.Parse(args[0]);
+Console.WriteLine($" initial number : {result}" );
+
+while (true)
+{
+    string? command = Console.ReadLine();
+    Console.WriteLine($" command : {command}" );
+}
